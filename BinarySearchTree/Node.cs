507ca0bec01ddd98110169321faacfcb8e788db1
pttiming <sean@pttiming.com>
2020-10-06 09:50:33 -0500

    Initial Classes Created

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+        public int data;
+        public Node leftNode;
+        public Node rightNode;
+
+        public Node(int data)
+        {
+            this.data = data;
+            
+        }
+    }
+}
