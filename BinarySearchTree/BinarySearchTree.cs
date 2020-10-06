3fdb3f8c5f54593af5b867d6b401527ee561162c
pttiming <sean@pttiming.com>
2020-10-06 11:33:40 -0500

    Building out Add and Search Methods

--- BinarySearchTree/BinarySearchTree.cs
+++ BinarySearchTree/BinarySearchTree.cs
@@ -8,6 +8,65 @@ namespace BinarySearchTree
 {
     class BinarySearchTree
     {
+        //Member Variables
         int value;
+        Node root;
+        
+
+        //Constructor
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+        //Methods
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+            Node current;
+            Node temp;
+
+            if (root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                current = root;
+                temp = current;
+
+                if(newNode.data < current.data)
+                {
+
+                }
+            }
+
+
+        }
+
+        public bool Search(int data)
+        {
+            Node current;
+            current = root;
+
+            while (true)
+            {
+                if (data == current.data)
+                    return true;
+                else if(data < current.data && current.leftNode != null)
+                {
+                    current = current.leftNode;
+                }
+                else if (data > current.data && current.rightNode != null)
+                {
+                    current = current.rightNode;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            
+        }
     }
+
 }
