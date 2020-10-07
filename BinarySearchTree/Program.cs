402766672d8195f52f2617df6394bca5d7474199
pttiming <sean@pttiming.com>
2020-10-07 08:33:54 -0500

    Search Method Added, MVP

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -11,17 +11,65 @@ namespace BinarySearchTree
     {
         static void Main(string[] args)
         {
-            BinarySearchTree tree = new BinarySearchTree();
-            tree.Add(10);
+            //BinarySearchTree tree = new BinarySearchTree();
+            //tree.Add(10);
             //tree.Add(15);
             //tree.Add(20);
             //tree.Add(25);
-            tree.Add(7);
+            //tree.Add(7);
             //tree.Add(3);
-            tree.Add(9);
-            tree.Add(8);
-            tree.Add(15);
-            Console.ReadLine();
+            //tree.Add(9);
+            //tree.Add(8);
+            //tree.Add(15);
+            //if(tree.Search(10))
+            //{
+            //    Console.WriteLine("Search Found");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not Found");
+            //}
+            //if (tree.Search(14))
+            //{
+            //    Console.WriteLine("Search Found");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not Found");
+            //}
+            //if (tree.Search(9))
+            //{
+            //    Console.WriteLine("Search Found");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not Found");
+            //}
+            //if (tree.Search(20))
+            //{
+            //    Console.WriteLine("Search Found");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not Found");
+            //}
+            //if (tree.Search(50))
+            //{
+            //    Console.WriteLine("Search Found");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not Found");
+            //}
+            //if (tree.Search(1))
+            //{
+            //    Console.WriteLine("Search Found");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not Found");
+            //}
+            //Console.ReadLine();
 
         }
     }
